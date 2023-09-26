8fce41fd561d66a89e4bfa44409a46f983300beb
farman khan <farmankhan@farmans-MacBook-Pro.local>
2023-09-26 23:56:57 +0800

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MultiModuleDemo"
+include(":app")
+include(":feature1")
+include(":feature2")
